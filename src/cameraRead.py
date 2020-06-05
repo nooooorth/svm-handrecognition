294f0f2947ae493032c366dc4cdd4f4703434952
wjy1478 <liuyizhu@hrbeu.edu.cn>
2020-06-05 17:59:27 +0800

    强制视频转换防止帧率过低，将两个窗口合并，未完成putText

--- src/cameraRead.py
+++ src/cameraRead.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from handSeg import ycrcbSeg
 
 def video_demo(cameraId):
@@ -19,20 +20,25 @@ def video_demo(cameraId):
 if __name__ == "__main__":
     cameraId = 0        # 摄像头ID
     cam = cv2.VideoCapture(cameraId)
+
+    # 强制视频格式转换，防止YUK格式帧率过低
+    fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+    cam.set(cv2.CAP_PROP_FOURCC, fourcc)
+    print('FrameWidth:', cam.get(cv2.CAP_PROP_FRAME_WIDTH), ', FrameHeight:', cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
     while(True):
         ref, frame = cam.read()
         if(not ref):
             print("Warn:cannot load camera!!!")
             exit(-1)
-        frame = frame[:, ::-1, :]
+        frame = frame[:, ::-1, :]       # 翻转图像
+        frameGray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)      # 为合并窗口做准备
+        _, crPic, ycrcbHand = ycrcbSeg(frame)       # ycrcb处理
 
-        _, crPic, ycrcbHand = ycrcbSeg(frame)
-
-        cv2.imshow("ycrcbHand", ycrcbHand)
-        cv2.imshow("BGR", frame)
-        if ((cv2.waitKey(30) & 0xff) == 27):
+        frameMerge = np.hstack((frameGray, ycrcbHand))      # 合并窗口
+        cv2.imshow("Camera",frameMerge)
+        # 加字未完成
+        if ((cv2.waitKey(30) & 0xff) == 27):        # 按下ESC退出
             cam.release()
             cv2.destroyAllWindows()
             exit(0)
-
-
