662bd6d1cd1882b62810fa81156e0e216dd67fb1
wjy1478 <liuyizhu@hrbeu.edu.cn>
2020-06-11 16:25:29 +0800

    增添生成矩阵函数，将图像压缩为1维，暂未实现合并，可通过excel合并

--- src/generateTxt.py
+++ src/generateTxt.py
@@ -1,4 +1,6 @@
 import os
+import cv2
+import numpy as np
 
 # dataPath：图片源文件夹
 # storePath: 输出文件夹
@@ -28,12 +30,26 @@ def mergeTxt(storePath):
             listMerge.write(item)
     listMerge.close()
 
+# 输入：图片文件夹路径，txt存放路径，label
+# 输出：输出（28*28）一维numpy数组 + label
+def generateMatrix(dataPath, storePath , i):
+    print("strat!")
+    data = np.empty(len(os.listdir()),785)
+    for fileName in os.listdir(dataPath+ "/" +str(i)):
+        # print("当前处理文件:",fileName)
+        img = cv2.imread(dataPath + "/" + fileName, 0)
+        img = cv2.resize(img,(28,28))
+        img = img.flatten()
+        data[i] = img           # 前面的值为
+        data[i][784] = i        # 最后一列=label
+    print("done!")
 
 if __name__ == "__main__":
     dataPath ="../data/crHand"
     storePath = "../data/txtList"
     i = 0
     while(i <= 9):
-        generateTxt(dataPath, storePath, i)
+        # generateTxt(dataPath, storePath, i)
+        generateMatrix(dataPath, storePath, i)
         i = i + 1
-    mergeTxt(storePath)
+    # mergeTxt(storePath)
