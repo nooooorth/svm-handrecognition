da437e2509e0f0f4378074b770d073377271e565
wjy1478 <liuyizhu@hrbeu.edu.cn>
2020-06-11 17:21:27 +0800

    无新增，待整理

--- src/readTxt.py
+++ src/readTxt.py
@@ -1,6 +1,12 @@
 import os
 import cv2
 import numpy as np
+from sklearn import svm
+from sklearn.model_selection import  train_test_split
+from sklearn.externals import joblib
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score,recall_score,f1_score,fbeta_score
+from sklearn.metrics import precision_recall_fscore_support, classification_report
 
 # 输入图片txt文件：文件名 标签
 # 返回文件名列表
@@ -21,6 +27,11 @@ def showImg(dataList):
 
 
 if __name__ == "__main__":
-    path="../data/txtList/list.txt"
-    dataList = readTxt(path)    # 读取txt中文件名保存至变量
-    # showImg(dataList)       # 显示图片
+    path = "../data/txtList/date.csv"     # 数据文件路径
+    data = np.loadtxt(path, dtype= int , delimiter=",")
+    # print(data)
+    # 最后一列（784列为标签）
+    x, y = np.split(data, (784,), axis= 1)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1, train_size=0.7)
+    print("done")
+
